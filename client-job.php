de9743ba01cbe96fc95890ce23e0fb62ca7985e0
bodhiintellect <94555905+bodhiintellect@users.noreply.github.com>
2021-12-11 07:03:20 +0530

    updates

---
+++ client-job.php
@@ -0,0 +1,11 @@
+<!-- Manifest -->
+<!-- Check Job -->
+<!-- Check Client -->
+<!-- Add Timer based on the client requirement -->
+<!-- Please -->
+<!-- Check if the Client Pleased -->
+<!-- Ok -->
+<?php 
+echo "Successfully Pleased Client";
+return;
+?>
